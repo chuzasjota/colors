1e5aaafd77e56638119f2d60032a5db62a396c9a
Jhonatan Vanegas Restrepo <chuzas.j@gmail.com>
2019-09-23 06:15:48 -0500

    Creación de componente + maquetación

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,25 @@
 <template>
   <div id="app">
-    <h1 class="title">Colores</h1>
-    <div class="column is-one-quarter" v-for="color in colors" :key="color.id">
-      <h2 class="title">{{color.name}}</h2>
+    <section class="hero is-dark">
+      <div class="hero-body">
+        <div class="container">
+          <h1 class="title has-text-centered">Colores</h1>
+        </div>
+      </div>
+    </section>
+    <div class="container">
+      <div class="columns is-desktop is-tablet is-mobile is-multiline is-centered">
+        <color v-for="color in colors" :key="color.id" v-bind:color="color"/>
+      </div>
+      <nav class="pagination" role="navigation" aria-label="pagination">
+        <a class="pagination-previous" v-on:click="changePage( page - 1 )">Anterior</a>
+        <ul class="pagination-list">
+          <li>
+            <a class="pagination-link is-current">{{page}}</a>
+          </li>
+        </ul>
+        <a class="pagination-next" v-on:click="changePage( page + 1 )">Siguiente</a>
+      </nav>
     </div>
   </div>
 </template>
@@ -10,24 +27,44 @@
 <script>
 //librerias
 import axios from "axios";
+//Componentes
+import Color from "./components/Color";
 
 export default {
   name: "App",
+  components: {
+    Color
+  },
   data() {
     return {
-      colors: []
+      colors: [],
+      page: 1,
+      pages: 1
     };
   },
-  created: async function() {
-    let result = axios
-      .get("https://reqres.in/api/colors")
+  created() {
+    this.fetch()
+  },
+  methods:{
+    fetch(){
+      const params = {
+        page: this.page
+      }
+      let result = axios
+      .get("https://reqres.in/api/colors", {params})
       .then(res => {
         this.colors = res.data.data;
-        console.log(this.colors)
+        this.pages = res.data.total_pages;
+        console.log(res.data);
       })
       .catch(err => {
         console.log(err);
       });
+    },
+    changePage(page){
+      this.page = page <= 0 || page > this.pages ? this.pages : page
+      this.fetch()
+    }
   }
 };
 </script>
---
+++ src/components/Color.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="column is-12-mobile is-4-desktop is-6-tablet">
+    <div class="card">
+      <header class="card-header">
+        <p class="card-header-title">{{color.year}}</p>
+      </header>
+      <div class="card-content">
+        <div class="content">
+          <h2>{{color.name}}</h2>
+          <p>{{color.color}}</p>
+        </div>
+      </div>
+      <footer class="card-footer">
+        <span>{{color.pantone_value}}</span>
+      </footer>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Color",
+  props: ['color']
+};
+</script>

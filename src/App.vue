8a4d047e2d8ff208365e7bd6a2ffb97c96bfd93c
Jhonatan Vanegas Restrepo <chuzas.j@gmail.com>
2019-09-23 04:40:16 -0500

    Obtener colores desde API

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,33 @@
 <template>
   <div id="app">
     <h1 class="title">Colores</h1>
+    <div class="column is-one-quarter" v-for="color in colors" :key="color.id">
+      <h2 class="title">{{color.name}}</h2>
+    </div>
   </div>
 </template>
 
 <script>
+//librerias
+import axios from "axios";
+
 export default {
-  name: "App"
+  name: "App",
+  data() {
+    return {
+      colors: []
+    };
+  },
+  created: async function() {
+    let result = axios
+      .get("https://reqres.in/api/colors")
+      .then(res => {
+        this.colors = res.data.data;
+        console.log(this.colors)
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
 };
 </script>
